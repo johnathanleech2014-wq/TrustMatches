0e7cfbf8973c25b1df3e26cb88558b21819958fa
johnathanleech2016-prog <johnathanleech2016@gmail.com>
2026-07-12 14:53:38 +0100

    Add React App component for TV Player

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TVPlayer from './components/TVPlayer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TVPlayer />
+    </div>
+  );
+}
+
+export default App;
